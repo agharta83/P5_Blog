e5de25bf06e8373b3497e8f5786c7f6aedcc2eb8
agharta83 <35310694+agharta83@users.noreply.github.com>
2020-05-23 14:30:00 +0200

    generate model

--- src/Models/CommentModel.php
+++ src/Models/CommentModel.php
@@ -2,4 +2,139 @@
 
 class CommentModel {
     
+    private $id;
+    private $created_on;
+    private $is_valid;
+    private $content;
+    private $respond_to;
+    private $post_id;
+    private $user_id;
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of created_on
+     */ 
+    public function getCreated_on()
+    {
+        return $this->created_on;
+    }
+
+    /**
+     * Set the value of created_on
+     *
+     * @return  self
+     */ 
+    public function setCreated_on($created_on)
+    {
+        $this->created_on = $created_on;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of is_valid
+     */ 
+    public function getIs_valid()
+    {
+        return $this->is_valid;
+    }
+
+    /**
+     * Set the value of is_valid
+     *
+     * @return  self
+     */ 
+    public function setIs_valid($is_valid)
+    {
+        $this->is_valid = $is_valid;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of respond_to
+     */ 
+    public function getRespond_to()
+    {
+        return $this->respond_to;
+    }
+
+    /**
+     * Set the value of respond_to
+     *
+     * @return  self
+     */ 
+    public function setRespond_to($respond_to)
+    {
+        $this->respond_to = $respond_to;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of post_id
+     */ 
+    public function getPost_id()
+    {
+        return $this->post_id;
+    }
+
+    /**
+     * Set the value of post_id
+     *
+     * @return  self
+     */ 
+    public function setPost_id($post_id)
+    {
+        $this->post_id = $post_id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_id
+     */ 
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */ 
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
 }
--- src/Models/PostModel.php
+++ src/Models/PostModel.php
@@ -2,4 +2,268 @@
 
 class PostModel {
     
+    private $id;
+    private $title;
+    private $chapo;
+    private $content;
+    private $created_on;
+    private $last_update;
+    private $img;
+    private $number_reviews;
+    private $published_date;
+    private $published;
+    private $slug;
+    private $category;
+    private $user_id;
+
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get the value of title
+     */ 
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set the value of title
+     *
+     * @return  self
+     */ 
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of chapo
+     */ 
+    public function getChapo()
+    {
+        return $this->chapo;
+    }
+
+    /**
+     * Set the value of chapo
+     *
+     * @return  self
+     */ 
+    public function setChapo($chapo)
+    {
+        $this->chapo = $chapo;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of content
+     */ 
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set the value of content
+     *
+     * @return  self
+     */ 
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of created_on
+     */ 
+    public function getCreated_on()
+    {
+        return $this->created_on;
+    }
+
+    /**
+     * Set the value of created_on
+     *
+     * @return  self
+     */ 
+    public function setCreated_on($created_on)
+    {
+        $this->created_on = $created_on;
+
+        return $this;
+    }
+
+
+
+    /**
+     * Get the value of last_update
+     */ 
+    public function getLast_update()
+    {
+        return $this->last_update;
+    }
+
+    /**
+     * Set the value of last_update
+     *
+     * @return  self
+     */ 
+    public function setLast_update($last_update)
+    {
+        $this->last_update = $last_update;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of img
+     */ 
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    /**
+     * Set the value of img
+     *
+     * @return  self
+     */ 
+    public function setImg($img)
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of number_reviews
+     */ 
+    public function getNumber_reviews()
+    {
+        return $this->number_reviews;
+    }
+
+    /**
+     * Set the value of number_reviews
+     *
+     * @return  self
+     */ 
+    public function setNumber_reviews($number_reviews)
+    {
+        $this->number_reviews = $number_reviews;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of published_date
+     */ 
+    public function getPublished_date()
+    {
+        return $this->published_date;
+    }
+
+    /**
+     * Set the value of published_date
+     *
+     * @return  self
+     */ 
+    public function setPublished_date($published_date)
+    {
+        $this->published_date = $published_date;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of published
+     */ 
+    public function getPublished()
+    {
+        return $this->published;
+    }
+
+    /**
+     * Set the value of published
+     *
+     * @return  self
+     */ 
+    public function setPublished($published)
+    {
+        $this->published = $published;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of slug
+     */ 
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * Set the value of slug
+     *
+     * @return  self
+     */ 
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of category
+     */ 
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * Set the value of category
+     *
+     * @return  self
+     */ 
+    public function setCategory($category)
+    {
+        $this->category = $category;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of user_id
+     */ 
+    public function getUser_id()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * Set the value of user_id
+     *
+     * @return  self
+     */ 
+    public function setUser_id($user_id)
+    {
+        $this->user_id = $user_id;
+
+        return $this;
+    }
 }
